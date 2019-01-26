3de61e752861ebffd3113d5327ae0b12511891ce
Yuri Pourre <yuripourre@gmail.com>
2019-01-26 16:26:02 -0200

    Update libgdx version to 1.9.9

--- src/test/java/com/harium/etyl/BaseEngineTest.java
+++ src/test/java/com/harium/etyl/BaseEngineTest.java
@@ -1,22 +1,30 @@
 package com.harium.etyl;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BaseEngineTest {
 
     private static final int WIDTH = 640;
     private static final int HEIGHT = 480;
 
+    @Before
+    public void setUp() {
+        Gdx.app = mock(Application.class);
+        when(Gdx.app.getType()).thenReturn(Application.ApplicationType.Desktop);
+    }
+
     @Test
     public void testInit() {
-        BaseEngine engine = new BaseEngine(640, 480) {
-        };
-
-        Assert.assertEquals(WIDTH, engine.w);
-        Assert.assertEquals(HEIGHT, engine.h);
-        Assert.assertNotNull(engine.core);
-        Assert.assertNotNull(engine.loaders);
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        Assert.assertNotNull(configuration);
     }
 
 }
